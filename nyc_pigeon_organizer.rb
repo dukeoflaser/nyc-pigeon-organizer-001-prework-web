6e716e53f4b7647ed3a6e6d5f8d1aec542d6c594
Nathaniel Miller <dukeoflaser@gmail.com>
2015-12-24 04:03:40 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  keys = data.keys
+
+  data[:gender].each do |gender, name_array|
+    keys.each do |key|
+      name_array.each do |name|
+        pigeon_list[name] = {} if pigeon_list[name] == nil
+        pigeon_list[name][key] = []
+      end
+    end  
+  end
+  
+  data.each do |k, h|
+    h.each do |subk, a|
+      a.each do |name|
+        pigeon_list[name][k] << subk.to_s
+      end
+    end
+  end
+
+  pigeon_list
 end
